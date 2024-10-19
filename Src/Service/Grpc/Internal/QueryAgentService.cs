4149c7b66ef7f11827ae051a12ad7ee766889360
techpology <alirashini2323@outlook.com>
2024-10-19 21:25:41 +0200

    client services to communicate with agents

---
+++ Src/Service/Grpc/Internal/QueryAgentService.cs
@@ -0,0 +1,40 @@
+using Gateway.Utils.Globals;
+using Grpc.Net.Client;
+using TpInternalService;
+
+namespace Agent.Services.Grpc;
+
+public class QueryAgentService
+{
+    private readonly QueryAgent.QueryAgentClient _client;
+
+    private readonly ILogger<QueryAgentService> _logger;
+    public QueryAgentService(ILogger<QueryAgentService> logger)
+    {
+        _logger = logger;
+
+        var channel = GrpcChannel.ForAddress(Globals.AgentsLoadbalancer);
+        _client = new QueryAgent.QueryAgentClient(channel);
+    }
+
+    public async Task<QueryResponse> QueryAsync(string key, float[] vectorData, int topK, float minThresh)
+    {
+        var vector = new Vector
+        {
+            Vec = { vectorData }
+        };
+
+        var request = new QueryRequest
+        {
+            Key = key,
+            Vector = vector,
+            TopK = topK,
+            MinThresh = minThresh
+        };
+
+        var response = await _client.QueryAsync(request);
+
+        return response;
+    }
+
+}
---
+++ Src/Service/Grpc/Internal/StoreBucketRowAgentService.cs
@@ -0,0 +1,40 @@
+using Gateway.Utils.Globals;
+using Google.Protobuf.Collections;
+using Grpc.Core;
+using Grpc.Net.Client;
+using TpInternalService;
+
+namespace Agent.Services.Grpc;
+
+public class StoreBucketRowAgentService
+{
+    private readonly StoreBucketRowAgent.StoreBucketRowAgentClient _client;
+    private readonly ILogger<QueryAgentService> _logger;
+
+    public StoreBucketRowAgentService(ILogger<QueryAgentService> logger)
+    {
+        _logger = logger;
+        var channel = GrpcChannel.ForAddress(Globals.AgentsLoadbalancer);
+        _client = new StoreBucketRowAgent.StoreBucketRowAgentClient(channel);
+    }
+
+    public async Task<bool> StoreBucketRowAsync(long id, float[] vectorData, byte[] chunkData, string key)
+    {
+        var vectorArr = new VectorArr
+        {
+            Vec = { vectorData }
+        };
+
+        var request = new StoreRequest
+        {
+            Id = id,
+            Vector = vectorArr,
+            Chunk = Google.Protobuf.ByteString.CopyFrom(chunkData),
+            Key = key
+        };
+
+        var response = await _client.StoreBucketRowAsync(request);
+
+        return response.Status;
+    }
+}
