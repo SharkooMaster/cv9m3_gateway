36f1990cbc0cff0ff95736a7b8b6b764a1a9a1d9
techpology <alirashini2323@outlook.com>
2024-11-09 18:03:42 +0100

    fixing external communication

--- Program.cs
+++ Program.cs
@@ -22,8 +22,8 @@ ConfigureServices(builder.Services);
 // Configure Kestrel to allow HTTP/2 without TLS
 builder.WebHost.ConfigureKestrel(options =>
 {
-    options.ListenLocalhost(5000, o => o.Protocols = HttpProtocols.Http2);
-    options.ListenLocalhost(5001, o => o.Protocols = HttpProtocols.Http1);
+    options.ListenLocalhost(8100, o => o.Protocols = HttpProtocols.Http2);
+    options.ListenLocalhost(8101, o => o.Protocols = HttpProtocols.Http1);
 });
 
 builder.Services.AddEndpointsApiExplorer();
