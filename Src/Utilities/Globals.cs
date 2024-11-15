05a019d8f382662fc3f34728aa201dea6b427990
techpology <alirashini2323@outlook.com>
2024-11-15 22:44:22 +0100

    update

--- Src/Utilities/Globals.cs
+++ Src/Utilities/Globals.cs
@@ -1,12 +1,19 @@
 using Gateway.Models;
+using Gateway.Utils.Misc;
 namespace Gateway.Utils.Globals;
 
 public static class Globals
 {
     // ETCD_CONFIG
-    public static string ETCD_ID = "";
-    public static string ETCD_VALUE = "";
-    public static long ETCD_LEASE_ID = -1;
+    //public static string ETCD_ID = "";
+    //public static string ETCD_VALUE = "";
+    //public static long ETCD_LEASE_ID = -1;
+
+    public static string GATEWAY_ID         = Misc.Misc.GenerateId();
+    public static string GATEWAY_VALUE      = "";
+    public static long   GATEWAY_LEASE_ID   = -1;
+
+    public static float MinThresh = 0.6f;
 
     // AGENTS_CONFIG
     public static string AgentsLoadbalancer = "http://agent-loadbalancer.default.svc.cluster.local:5000";
---
+++ Src/Utilities/Misc.cs
@@ -0,0 +1,60 @@
+
+using Google.Protobuf;
+using System.Text.Json;
+using System.Net;
+
+namespace Gateway.Utils.Misc;
+
+public static class Misc
+{
+    public static string GenerateId()
+    {
+        return Guid.NewGuid().ToString();
+    }
+
+    public static string GetServiceInfo(string service_name, string service_id)
+    {
+        string _host = GetLocalIPAddress();
+        string _port = "5000";
+        string _version = "v1.0.0";
+        string _env = "dev";
+
+        var serviceInfo = new {
+            id          = service_id,
+            name        = service_name,
+            host        =  _host,
+            port        = _port,
+            url         = $"http://{_host}:{_port}",
+            healthCheck = $"http://{_host}:{_port}/health",
+            version     = _version,
+            metadata    = new {
+                environment = _env,
+            }
+        };
+
+        return JsonSerializer.Serialize(serviceInfo);
+    }
+
+    private static string GetLocalIPAddress()
+    {
+        string localIP = string.Empty;
+        try
+        {
+            var host = Dns.GetHostEntry(Dns.GetHostName());
+            foreach (var ip in host.AddressList)
+            {
+                if (ip.AddressFamily == System.Net.Sockets.AddressFamily.InterNetwork)
+                {
+                    localIP = ip.ToString();
+                    break;
+                }
+            }
+        }
+        catch (Exception)
+        {
+            localIP = "127.0.0.1"; // Fallback to localhost if there's an issue
+        }
+
+        return localIP;
+    }
+}
