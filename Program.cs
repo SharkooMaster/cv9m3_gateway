b7807964382c7fa1826caa41c09bcf5e58f02fc1
sharku <alirashini2323@outlook.com>
2025-02-25 21:11:57 +0100

    debugging

--- Program.cs
+++ Program.cs
@@ -30,7 +30,7 @@ builder.WebHost.ConfigureKestrel(options =>
 {
     options.ListenAnyIP(5000, o => o.Protocols = HttpProtocols.Http2);
     options.ListenAnyIP(5001, o => o.Protocols = HttpProtocols.Http1);
-    options.Limits.MaxRequestBodySize = 52428800;
+    options.Limits.MaxRequestBodySize = 1024 * 1024 * 1024;
 });
 
 builder.Services.AddEndpointsApiExplorer();
