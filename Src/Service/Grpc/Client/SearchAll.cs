5c140cfec446a1f14d5ca2647f0690c16d560132
sharku <alirashini2323@outlook.com>
2025-02-27 22:59:46 +0100

    update

--- Src/Service/Grpc/Client/SearchAll.cs
+++ Src/Service/Grpc/Client/SearchAll.cs
@@ -26,25 +26,27 @@ public class SearchAllService : GatewayService.GatewayService.GatewayServiceBase
         ConcurrentBag<int> FoundBag = new ConcurrentBag<int>();
 
         // Create a list of tasks instead of using Parallel.For with async lambdas
-        var tasks = new List<Task>();
 
         for (int i = 0; i < request.QueryObjects.Count; i++)
         {
             int index = i; // capture the loop variable
-            tasks.Add(Task.Run(async () =>
-            {
-                SearchVector_Req req = new SearchVector_Req();
-                req.Vector.AddRange(request.QueryObjects[index].Vector);
-                req.Bitstring = request.QueryObjects[index].BucketString;
-                req.K = Globals.K;
-                req.MinimumSimilarity = Globals.MinThresh;
-                await AgnetaHandler.Log(0, request.QueryObjects[index].BucketString);
-                char[] req_bitstring = req.Bitstring.ToCharArray();
+            SearchVector_Req req = new SearchVector_Req();
+            req.Vector.AddRange(request.QueryObjects[index].Vector);
+            req.Bitstring = request.QueryObjects[index].BucketString;
+            req.K = Globals.K;
+            req.MinimumSimilarity = Globals.MinThresh;
+            await AgnetaHandler.Log(0, request.QueryObjects[index].BucketString);
+            char[] req_bitstring = req.Bitstring.ToCharArray();
 
-                SearchVector_Result res = new SearchVector_Result();
-                Stopwatch sw = new Stopwatch();
-                sw.Start();
-                for (int j = 0; j < 64; j++)
+            SearchVector_Result res = new SearchVector_Result();
+            ConcurrentBag<SearchVectorObject> svOjects = new ConcurrentBag<SearchVectorObject>();
+
+            Stopwatch sw = new Stopwatch();
+            sw.Start();
+            var tasks = new List<Task>();
+            for (int j = 0; j < 64; j++)
+            {
+                tasks.Add(Task.Run(async () =>
                 {
                     if(j > 0)
                     {
@@ -60,66 +62,67 @@ public class SearchAllService : GatewayService.GatewayService.GatewayServiceBase
                     {
                         res.TargetIp = _res.TargetIp;
                     }
-                }
-                sw.Stop();
-                Console.WriteLine($"{index}: took {sw.ElapsedMilliseconds}ms to search for buckets");
+                }));
+            }
+            await Task.WhenAll(tasks);
+            sw.Stop();
+            Console.WriteLine($"{index}: took {sw.ElapsedMilliseconds}ms to search for buckets");
 
-                if (res.Results.Count == 0 && !request.QueryObjects[index].IsNeighbour)
+            if (res.Results.Count == 0 && !request.QueryObjects[index].IsNeighbour)
+            {
+                await AgnetaHandler.Log(0, $"Storing [{index}]");
+                // Save
+                StoreVector_Req svecReq = new StoreVector_Req
                 {
-                    await AgnetaHandler.Log(0, $"Storing [{index}]");
-                    // Save
-                    StoreVector_Req svecReq = new StoreVector_Req
-                    {
-                        TargetIp = res.TargetIp,
-                        Bitstring = req.Bitstring
-                    };
-                    svecReq.Vector.AddRange(req.Vector);
+                    TargetIp = res.TargetIp,
+                    Bitstring = req.Bitstring
+                };
+                svecReq.Vector.AddRange(req.Vector);
 
-                    M_Meta meta = new M_Meta
-                    {
-                        chunk = Convert.ToBase64String(request.QueryObjects[index].Chunk.ToByteArray())
-                    };
-                    svecReq.Metadata = JsonConvert.SerializeObject(meta);
-                    await AgnetaHandler.Log(0, $"[{index}] Storing new vector");
-                    ulong vectorIndex = svec.Store(svecReq).Id;
-                    await AgnetaHandler.Log(0, $"[{index}] Stored new vector: {svecReq.Metadata[..20]}");
-
-                    resultsBag.Add(new QueryResponseObject
-                    {
-                        Id = Convert.ToUInt64(req.Bitstring, 2),
-                        IdPost = vectorIndex,
-                        Index = request.QueryObjects[index].Index,
-                        Similarity = 1,
-                        Chunk = request.QueryObjects[index].Chunk
-                    });
-                }
-                else
+                M_Meta meta = new M_Meta
                 {
-                    for (int j = 0; j < res.Results.Count; j++)
+                    chunk = Convert.ToBase64String(request.QueryObjects[index].Chunk.ToByteArray())
+                };
+                svecReq.Metadata = JsonConvert.SerializeObject(meta);
+                await AgnetaHandler.Log(0, $"[{index}] Storing new vector");
+                ulong vectorIndex = svec.Store(svecReq).Id;
+                await AgnetaHandler.Log(0, $"[{index}] Stored new vector: {svecReq.Metadata[..20]}");
+
+                resultsBag.Add(new QueryResponseObject
+                {
+                    Id = Convert.ToUInt64(req.Bitstring, 2),
+                    IdPost = vectorIndex,
+                    Index = request.QueryObjects[index].Index,
+                    Similarity = 1,
+                    Chunk = request.QueryObjects[index].Chunk
+                });
+            }
+            else
+            {
+                for (int j = 0; j < res.Results.Count; j++)
+                {
+                    await AgnetaHandler.Log(0, $"[{index}]:[{j}] Sim: {res.Results[j].SimilarityRate}");
+                    if (res.Results[j].SimilarityRate >= Globals.MinThresh)
                     {
-                        await AgnetaHandler.Log(0, $"[{index}]:[{j}] Sim: {res.Results[j].SimilarityRate}");
-                        if (res.Results[j].SimilarityRate >= Globals.MinThresh)
+                        await AgnetaHandler.Log(0, $"[{index}]:[{j}] Match found, preparing QRO");
+                        JObject meta = JObject.Parse(res.Results[j].Metadata);
+                        await AgnetaHandler.Log(0, $"[{index}]:[{j}] Meta parsed");
+                        Google.Protobuf.ByteString chunk = ByteString.CopyFrom(
+                            Convert.FromBase64String(meta["chunk"]?.ToString()));
+                        await AgnetaHandler.Log(0, $"[{index}]:[{j}] Chunk copied");
+
+                        resultsBag.Add(new QueryResponseObject
                         {
-                            await AgnetaHandler.Log(0, $"[{index}]:[{j}] Match found, preparing QRO");
-                            JObject meta = JObject.Parse(res.Results[j].Metadata);
-                            await AgnetaHandler.Log(0, $"[{index}]:[{j}] Meta parsed");
-                            Google.Protobuf.ByteString chunk = ByteString.CopyFrom(
-                                Convert.FromBase64String(meta["chunk"]?.ToString()));
-                            await AgnetaHandler.Log(0, $"[{index}]:[{j}] Chunk copied");
-
-                            resultsBag.Add(new QueryResponseObject
-                            {
-                                Id = res.Results[j].Id,
-                                IdPost = res.Results[j].Index,
-                                Index = request.QueryObjects[index].Index,
-                                Similarity = res.Results[j].SimilarityRate,
-                                Chunk = chunk
-                            });
-                            await AgnetaHandler.Log(0, $"[{index}]:[{j}] Result added {resultsBag.Count}");
-                        }
+                            Id = res.Results[j].Id,
+                            IdPost = res.Results[j].Index,
+                            Index = request.QueryObjects[index].Index,
+                            Similarity = res.Results[j].SimilarityRate,
+                            Chunk = chunk
+                        });
+                        await AgnetaHandler.Log(0, $"[{index}]:[{j}] Result added {resultsBag.Count}");
                     }
                 }
-            }));
+            }
         }
 
         // Await all the tasks to complete
