4fab25d309a8315074b7bfb22edf6e9b16023256
sharku <alirashini2323@outlook.com>
2025-04-18 11:27:56 +0200

    update

--- Program.cs
+++ Program.cs
@@ -39,6 +39,10 @@ builder.WebHost.ConfigureKestrel(options =>
     options.ListenAnyIP(5000, o => o.Protocols = HttpProtocols.Http2);
     options.ListenAnyIP(5001, o => o.Protocols = HttpProtocols.Http1);
     options.Limits.MaxRequestBodySize = 1024 * 1024 * 1024;
+    
+    options.Limits.KeepAliveTimeout = TimeSpan.FromMinutes(10);
+    options.Limits.Http2.KeepAlivePingDelay = TimeSpan.FromSeconds(30);
+    options.Limits.Http2.KeepAlivePingTimeout = TimeSpan.FromSeconds(60);
 });
 
 builder.Services.AddEndpointsApiExplorer();
