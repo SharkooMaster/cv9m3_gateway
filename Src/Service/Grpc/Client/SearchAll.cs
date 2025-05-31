728d7bd62346e6f81ea4e351bbdb251e44faf95d
sharku <alirashini2323@outlook.com>
2025-05-31 23:33:08 +0200

    debugging

--- Src/Service/Grpc/Client/SearchAll.cs
+++ Src/Service/Grpc/Client/SearchAll.cs
@@ -145,8 +145,8 @@ public class SearchAllService : GatewayService.GatewayService.GatewayServiceBase
         // Store
         for (int i = 0; i < ToStore.Count; i++)
         {
-            byte[] _chunk = indexedChunks[ToStore[i].Results[0].Index].Chunk.ToArray();
-            float[] _vec = indexedChunks[ToStore[i].Results[0].Index].Vector.ToArray();
+            byte[] _chunk = indexedChunks[ToStore[i].Results[0].I].Chunk.ToArray();
+            float[] _vec = indexedChunks[ToStore[i].Results[0].I].Vector.ToArray();
 
             _ = NetworkFileStorageHandler.StoreVector("", new M_Data() {
                 chunk = _chunk,
